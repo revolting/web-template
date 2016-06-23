bdba853d059e9ffb58192fb59d8ecf7102133f4e
Jennifer Fong <error@error.media>
2016-06-23 11:05:06 -0700

    moved handlers to main

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"github.com/unrolled/render"
 )
 
 var (
 	httpPort 	= flag.String("port", ":8080", "Listen address")
 	serverEnv 	= flag.Bool("isDev", true, "Server environment mode")
+	r = render.New(render.Options{
+		Directory: "templates",
+		Extensions: []string{".html"},
+		IsDevelopment: *serverEnv,
+	})
 )
 
 func main() {
@@ -18,4 +25,20 @@ func main() {
 	router.PathPrefix("/media/").Handler(http.StripPrefix("/media/", http.FileServer(http.Dir("./media/"))))
 
 	log.Fatal(http.ListenAndServe(*httpPort, router))
+}
+
+func Index(w http.ResponseWriter, req *http.Request) {
+	r.HTML(w, http.StatusOK, "index", nil)
+}
+
+func Directory(w http.ResponseWriter, req *http.Request) {
+	r.HTML(w, http.StatusOK, "directory", nil)
+}
+
+func Authenticate(w http.ResponseWriter, req *http.Request) {
+	r.HTML(w, http.StatusOK, "authenticate", nil)
+}
+
+func Validate(w http.ResponseWriter, req *http.Request) {
+	r.HTML(w, http.StatusOK, "validate", nil)
 }
