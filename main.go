3313b76b622bcb3efc0f73b099c73e61dd0e59b3
Jen <jfong@mozilla.com>
2016-06-23 22:24:37 -0700

    indentation fix

--- main.go
+++ main.go
@@ -12,13 +12,13 @@ import (
 )
 
 var (
-	httpPort 		= flag.String("port", ":8080", "Listen address")
-	serverEnv 		= flag.Bool("isDev", true, "Server environment mode")
-	twilioSid 		= flag.String("twilioSid", "111", "Twilio SID")
-	twilioToken 	= flag.String("twilioToken", "111", "Twilio token")
+	httpPort		= flag.String("port", ":8080", "Listen address")
+	serverEnv		= flag.Bool("isDev", true, "Server environment mode")
+	twilioSid		= flag.String("twilioSid", "111", "Twilio SID")
+	twilioToken		= flag.String("twilioToken", "111", "Twilio token")
 	twilioPhone		= flag.String("twilioPhone", "+15555555", "Twilio phone number")
-	cookieSecret 	= flag.String("cookie", "secret", "Session cookie secret")
-	store 			= sessions.NewCookieStore([]byte(*cookieSecret))
+	cookieSecret	= flag.String("cookie", "secret", "Session cookie secret")
+	store			= sessions.NewCookieStore([]byte(*cookieSecret))
 
 	r				= render.New(render.Options{
 						Directory: "templates",
